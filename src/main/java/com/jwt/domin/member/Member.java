e7c0bf810f1623c22e94084730fe7bcc98bf8033
bandall <jsm5315@naver.com>
2023-11-21 20:15:52 +0900

    feat: Member 패키지 생성

---
+++ src/main/java/com/jwt/domin/member/Member.java
@@ -0,0 +1,48 @@
+package com.jwt.domin.member;
+
+import com.jwt.domin.util.BaseTimeEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(exclude = {"password"})
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(unique = true, length = 50)
+    private String email;
+
+    @Column(length = 100)
+    private String password;
+
+    @Column(length = 50)
+    private String username;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Builder
+    public Member(String email, String password, String username, Role role) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.role = role;
+    }
+
+}
---
+++ src/main/java/com/jwt/domin/member/Role.java
@@ -0,0 +1,7 @@
+package com.jwt.domin.member;
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ANONYMOUS,
+    ROLE_ADMIN
+}
