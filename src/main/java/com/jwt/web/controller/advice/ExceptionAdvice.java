037fab2555f61f26d0f2e051e2656dcd4748c5fd
bandall <jsm5315@naver.com>
2023-11-21 22:48:45 +0900

    feat: ExceptionAdvice Exception 추가

--- src/main/java/com/jwt/web/controller/advice/ExceptionAdvice.java
+++ src/main/java/com/jwt/web/controller/advice/ExceptionAdvice.java
@@ -4,7 +4,10 @@ import com.jwt.web.controller.json.ApiResponseJson;
 import com.jwt.web.controller.json.ResponseStatusCode;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.TypeMismatchException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -26,6 +29,14 @@ public class ExceptionAdvice {
     public ApiResponseJson handleBadRequestException(Exception e) {
         return new ApiResponseJson(HttpStatus.BAD_REQUEST, ResponseStatusCode.WRONG_PARAMETER,
                 Map.of("errMsg", e.getMessage()));
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({MethodArgumentNotValidException.class, TypeMismatchException.class,
+            HttpMessageNotReadableException.class})
+    public ApiResponseJson handleBadRequestBody() {
+        return new ApiResponseJson(HttpStatus.BAD_REQUEST, ResponseStatusCode.WRONG_PARAMETER,
+                Map.of("errMsg", "잘못된 요청입니다."));
     }
 
 }
